6efdcb9e98116f99b842def5cac587f00ba337b0
Suyash <dubli.suyash2202@gmail.com>
2020-06-13 20:56:46 +0530

    database change

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/',(req,res)=>{
-	res.send(database.users);
-});
+app.get('/',(req,res)=>{res.send('it is working')});
 
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
 
